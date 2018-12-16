28989142c285037ec501ccc46b186c185218fd71
Ying <yanying1981@163.com>
2018-12-16 23:11:45 +0800

    20181216

--- econfig/install.sh
+++ econfig/install.sh
@@ -1,6 +1,9 @@
+if [ -f "$HOME/.zshrc" ]; then
+    echo "source $HOME/.vim/econfig/init.sh" >> $HOME/.zshrc
+fi
+
 if [ -d "$HOME/.config" ]; then
     ln -s ~/.vim ~/.config/nvim
-    ln -s ~/.vim/vimrc ~/.config/nvim/init.vim
 fi
-ln -s ~/.vim/econfig/.tmux.conf ~/.tmux.conf
 
+ln -s ~/.vim/econfig/.tmux.conf ~/.tmux.conf
